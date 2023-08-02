28b21919a890a979bdcab82bf6880c4c7cce37a4
irinakhudieva <119666740+irinakhudieva@users.noreply.github.com>
2023-08-02 22:58:44 +0300

    Update Repositories.jsx

--- src/components/UserPage/Repositories.jsx
+++ src/components/UserPage/Repositories.jsx
@@ -6,13 +6,14 @@ import { userService } from '../../API/API';
 import Loader from '../UI/loader/Loader';
 
 const Repositories = ({username}) => {
-    const [repos, setRepos] = useState({})
+    const [repos, setRepos] = useState([])
     const [currentPage, setCurrentPage] = useState(1);
     const [page, setPage] = useState(5);
 
     const getData = async () => {
         try {
             const response = await userService.getRepositories(username);
+            console.log(response);
             setRepos(response.data);
         } catch (error) {
             console.log('error', error)
